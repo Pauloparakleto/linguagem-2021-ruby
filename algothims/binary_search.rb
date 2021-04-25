139dbdfce836d7cf0206eb7cbb1f8d10b8fd1839
paulo-felipe <alparakleto@gmail.com>
2021-04-24 22:08:16 -0300

    feat(binary_search.rb): it runs for 2

--- algothims/binary_search.rb
+++ algothims/binary_search.rb
@@ -1,20 +1,39 @@
 list = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-number = 38
+number = 2
 
 def binary_search(list, number)
+  @first_part = ""
+  @second_part = ""
   list_standard = list
+  list = list
   number = number
   p "Call binary search"
   p "list is #{list}"
   return nil if list.nil?
 
-  first_part = take_first_half(list)
-  second_part = take_second_half(list)
+  if number <= take_first_half(list).last
+    @first_part = take_first_half(list)
 
-  if number == first_part.last
-    p "sss"
-    return list_standard.index number
+    if number == @first_part.last
+      p "sss"
+      return list_standard.index number
+    else
+      list = @first_part
+      binary_search(list, number)
+    end
+  else
+    @second_part = take_second_half(list)
+    if number == @second_part.first
+      p "sss"
+      return list_standard.index number
+    else
+      list = second_part
+      binary_search(list, number)
+    end
   end
+=begin
+  first_part = take_first_half(list) if number < take_first_half(list).last
+  second_part = take_second_half(list) if number > take_second_half(list).first
 
   if number == second_part.first
     p "sss"
@@ -30,7 +49,7 @@ def binary_search(list, number)
     list = first_part
     binary_search(list, number)
   end
-
+=end
 end
 
 def take_first_half(list)
