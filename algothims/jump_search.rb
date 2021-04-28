f858f9bc4618068a2b536449c2634bf2e4a8bd42
paulo-felipe <alparakleto@gmail.com>
2021-04-28 19:01:21 -0300

    feat(jump_search.rb): first version all running

--- algothims/jump_search.rb
+++ algothims/jump_search.rb
@@ -1,5 +1,6 @@
 array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-number = 55
+number = 610
+
 class JumpSearch
   def initialize(array, number)
     puts "init jump search"
@@ -10,15 +11,37 @@ class JumpSearch
 
   def search
     puts "In search"
-    index = 0
-    while index <= @array_length
-      puts "Index is: #{index}."
-      index += jump(@array_length)
+    @index = 0
+    while @index <= @array_length
+      puts "Index is: #{@index}."
+      if @array[@index] == @number
+        return @array.index @number
+      end
+      if @array[@index] > @number
+        @index += jump(@array_length)
+      else
+        @index -= jump(@array_length)
+        linear_search(array: @array, start_index: @index, number: @number)
+      end
+
     end
 
   end
 
-  #TODO: Comparison function and calling linear search
+  def linear_search(array:, start_index:, number:)
+    array = array
+    @index = start_index
+    number = number
+
+    array.length.times do
+
+      if array[@index] == number
+        return array.index number
+      end
+      @index += 1
+    end
+    return nil
+  end
 
   def jump(array_length)
     "jump array"
