29073e4dbcb7b9d1415e6c83f895fb6127eb9aa7
paulo-felipe <alparakleto@gmail.com>
2021-05-05 20:40:51 -0300

    chore(manixime_array.rb): remove comments

--- algothims/maximize_array.rb
+++ algothims/maximize_array.rb
@@ -8,54 +8,35 @@ class MaximizeArray
   end
 
   def maximize
-    #Todo: o número 5, mesmo sendo positivo sofre alteração para negativo e não consegue ser positivo novamente, pois
-    # são usadas apenas k=3 tentativas. Por issom não é possível que a soma maxima seja 30.
-    # Todo: refatorar usando select funçao
     @score = 0
-    p "score is #{@score}"
     sorted_array = @array.sort
-    p "sorted array #{sorted_array}"
-
     @negative_array = sorted_array.select { |number| number < 0 }
-    p "negative array #{@negative_array}"
     @positive_array = sorted_array.select { |number| number > 0 }
-    p "positive array #{@positive_array}"
+
     @negative_array.map! do |number|
-      p "In negative array block"
-      p "Score was #{@score}"
-      p "number was #{number}"
       number = number * (-1)
-      p "Become #{number}"
       @score += 1
-      p "Now score is #{@score}"
       if @score == @k
-        p "Score reach k possibilities"
-        p "return sum"
         return @negative_array.sum + @positive_array.sum
       end
       number
     end
 
-    p "negative array now is: #{@negative_array} "
-
     @zero_array = sorted_array.select { |number| number == 0 }
+
     @zero_array.map! do
-      p 'There is a zero number'
       return @negative_array.sum + @positive_array.sum
     end
     @positive_array.map! do |number|
       if @k.even?
-        p 'k is even return sum'
         number
       else
         if @score < @k
-          p 'score less than k'
           @score = @k
           -number
         else
           number
         end
-
       end
     end
     @positive_array.sum + @negative_array.sum
