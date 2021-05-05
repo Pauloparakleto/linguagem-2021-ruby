b45ca2de41b0f01a5a61d38161ba72dfbd9235d4
paulo-felipe <alparakleto@gmail.com>
2021-05-05 09:02:18 -0300

    refactor(manixime_array.rb): with map method.

--- algothims/maximize_array.rb
+++ algothims/maximize_array.rb
@@ -1,5 +1,5 @@
-array = [9, 8, 8, 5]
-k = 3
+array = [-2, 0, 5, -1, 2]
+k = 4
 
 class MaximizeArray
   def initialize(array, k)
@@ -11,33 +11,48 @@ class MaximizeArray
     #Todo: o número 5, mesmo sendo positivo sofre alteração para negativo e não consegue ser positivo novamente, pois
     # são usadas apenas k=3 tentativas. Por issom não é possível que a soma maxima seja 30.
     # Todo: refatorar usando select funçao
-    @index = -1
     @score = 0
+    p "score is #{@score}"
     sorted_array = @array.sort
-    @k.times do
-      break if @score == @k
-      @index += 1
-      if sorted_array[@index] < 0
-        sorted_array[@index] = sorted_array[@index] * (-1)
-        @score += 1
-      elsif sorted_array[@index] == 0
-        return sorted_array.sum
-      else
-        #Todo tratar o caso do número ser positivo e ser o mínimo dos positivos
-        remaining_moves = @k - @score
-        if remaining_moves.even?
-          return sorted_array.sum
-        else
-          only_positives = sorted_array.select { |number| number > 0 }
-          only_positives[0] * (-1)
-          only_negatives = sorted_array.select { |number| number < 0 }
+    p "sorted array #{sorted_array}"
 
-          return only_positives.sum + only_negatives.sum
-        end
+    @negative_array = sorted_array.select { |number| number < 0 }
+    p "negative array #{@negative_array}"
+    @positive_array = sorted_array.select { |number| number > 0 }
+    p "positive array #{@positive_array}"
+
+    @negative_array.map! do |number|
+      p "In negative array block"
+      p "Score was #{@score}"
+      p "number was #{number}"
+      number = number * (-1)
+      p "Become #{number}"
+      @score += 1
+      p "Now score is #{@score}"
+      if @score == @k
+        p "Score reach k possibilities"
+        p "return sum"
+        return @negative_array.sum + @positive_array.sum
+      end
+
+    end
+
+    @zero_array = sorted_array.select { |number| number == 0 }
+    @zero_array.map! do
+      p 'There is a zero number'
+      return @negative_array.sum + @positive_array.sum
+    end
+    @positive_array.map! do |number|
+      if @k.even?
+        p 'k is even return sum'
+        return @positive_array.sum + @negative_array.sum
+      else
+        number * (-1)
+        @score += 1
+        return @positive_array.sum + @negative_array.sum if @score == @k
       end
     end
   end
-
 end
 
 result = MaximizeArray.new(array, k).maximize
